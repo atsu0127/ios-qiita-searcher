f64dc50e376d386ff3ae829ddfe37a546195097f
Atsutomo Tabata <atabata@yahoo-corp.jp>
2022-03-19 17:03:17 +0900

    Initial Commit

---
+++ ios-qiita-searcher/ios_qiita_searcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_qiita_searcherApp.swift
+//  ios-qiita-searcher
+//
+//  Created by atabata on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct ios_qiita_searcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
